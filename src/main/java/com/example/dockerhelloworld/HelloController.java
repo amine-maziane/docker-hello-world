203f3430dfd81eef184f8eb0f2b18ae960083d1e
amine.maziane <amine.maziane@bakkt.com>
2021-12-28 13:31:37 -0500

    inital commit

---
+++ src/main/java/com/example/dockerhelloworld/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.dockerhelloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hi() {
+        return "What's up world !!!!";
+    }
+}
